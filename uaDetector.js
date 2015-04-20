cd7e9ec3c31f13a723e5e4149e084a69088bd36b
van.zheng <van.zheng@vipshop.com>
2015-04-20 10:31:10 +0800

    check in file

---
+++ uaDetector.js
@@ -0,0 +1,104 @@
+/*!
+ * uaDetector
+ *
+ * @author      Van Zheng (zgbjili2009@126.com)
+ * @copyright   Copyright (c) Van Zheng.
+ * @license     This uaDetector is licensed under the MIT licenses.
+ * @version     Version 0.8.0
+ *
+ */
+
+(function(root) {
+    var ua = window.navigator.userAgent.toLowerCase(),
+    	matches,
+    	version = '0',
+    	device = 'unknown',
+    	os = 'unknown',
+    	uaDetector;
+
+    // Browser info
+    // Opera userAgent contains `chrome`, `safari` keywords
+    if (ua.indexOf('opera') > -1 || ua.indexOf('opr') > -1) { 
+    	browser = 'opera';
+
+    	matches = /[opera|opr][\s\/]+([\d]+)/.exec(ua);
+    	version = matches ? matches[1] : '0';
+    }
+    // Chrome userAgent contains `chrome`, `safari` keywords
+    else if (ua.indexOf('chrome') > -1) { 
+    	browser = 'chrome';
+
+    	matches = /chrome[\s\/]+([\d]+)/.exec(ua);
+    	version = matches ? matches[1] : '0';
+    }
+    else if (ua.indexOf('safari') > -1) {
+    	browser = 'safari';
+
+    	matches = /safari[\s\/]+([\d]+)/.exec(ua);
+    	version = matches ? matches[1] : '0';
+    }
+    else if (ua.indexOf('firefox') > -1) {
+    	browser = 'firefox';
+
+    	matches = /firefox[\s\/]+([\d]+)/.exec(ua);
+    	version = matches ? matches[1] : '0';
+    }
+    else if (ua.indexOf('msie') > -1 || ua.indexOf('trident') > -1) {
+    	browser = 'msie';
+
+    	matches = /(msie|rv:?)\s?([\d\.]+)/.exec(ua);
+        version = matches ? matches[2] : '0';
+    }
+
+    // platform info
+    if (ua.indexOf('iphone') > -1 || ua.indexOf('ipad') > -1 || ua.indexOf('ipod') > -1) {
+        device = 'mobile';
+        os = 'ios';
+    } else if (ua.indexOf('android') > -1) {
+        device = 'mobile';
+        os = 'android';
+    } else if (ua.indexOf('windows phone') > -1) {
+        device = 'mobile';
+        os = 'windows';
+    } else if (ua.indexOf('blackberry') > -1) {
+        device = 'mobile';
+        os = 'blackberry';
+    } else if (ua.indexOf('symbian') > -1) {
+        device = 'mobile';
+        os = 'symbian';
+    } else if (ua.indexOf('windows') > -1) {
+        device = 'desktop';
+        os = 'windows';
+    } else if (ua.indexOf('macintosh') > -1) {
+        device = 'macintosh';
+        os = 'macos';
+    } else if (ua.indexOf('linux') > -1) {
+        device = 'desktop';
+        os = 'linux';
+    }
+
+    // Rendering engine
+    if (userAgent.indexOf('webkit') > -1) {
+        engine = 'webkit';
+    } else  if (userAgent.indexOf('trident') > -1) {
+        engine = 'trident';
+    } else if (userAgent.indexOf('presto') > -1) {
+        engine = 'presto';
+    } else if (userAgent.indexOf('khtml') > -1) {
+        engine = 'khtml';
+    } else if (userAgent.indexOf('gecko') > -1) {
+        engine = 'gecko';
+    } else {
+        engine = 'unknown';
+    }
+
+	uaDetector = {
+		browser: browser,
+		version: version,
+		device: device,
+		os: os,
+		engine: engine
+	}    
+
+	root.uaDetector = uaDetector;
+})(window);
